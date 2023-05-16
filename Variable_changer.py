62e250a67c70672389be928426b643fab82b62a0
htmlcoder1562 <110137761+htmlcoder1562@users.noreply.github.com>
2023-05-16 13:57:34 -0500

    Update Variable_changer.py
    Shown test project

--- Variable_changer.py
+++ Variable_changer.py
@@ -2,9 +2,11 @@ import scratchattach as scratch3
 Uname=input("What is your Username?")
 Password=input("What is your password?")
 ProjID=input("What is the project id?")
-varname=input("What is the name of the variable/cloud variable?")
+varname=input("What is the name of the cloud variable?")
 value=input("What is the value?")
 session=scratch3.login(Uname,Password)
 conn=session.connect_cloud(ProjID)
 conn.set_var(varname, value)
 conn.disconnect()
+# Test this at: https://scratch.mit.edu/projects/851418630/
+# If you use that, then for the project ID, use this: 851418630
