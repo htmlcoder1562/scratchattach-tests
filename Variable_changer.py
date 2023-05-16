7f231f85d7deebc88bf04cd5d53d1320f3371db9
htmlcoder1562 <110137761+htmlcoder1562@users.noreply.github.com>
2023-05-16 14:31:13 -0500

    Update Variable_changer.py
    FIXED!!!!!

--- Variable_changer.py
+++ Variable_changer.py
@@ -1,12 +1,17 @@
 import scratchattach as scratch3
-Uname=input("What is your Username?")
-Password=input("What is your password?")
-ProjID=input("What is the project id?")
-varname=input("What is the name of the cloud variable?")
-value=input("What is the value?")
-session=scratch3.login(Uname,Password)
-conn=session.connect_cloud(ProjID)
-conn.set_var(varname, value)
-conn.disconnect()
+Done=0
+sessionID=input("What is your Session ID? You can find it at: https://github.com/TimMcCool/scratchattach/wiki/Get-your-session-id\n")
+uname=input("What is your username?\n")
+while Done==0:
+    ProjID=input("What is the project id?\n")
+    varname=input("What is the name of the cloud variable?\n")
+    value=input("What is the value? note: numbers only\n")
+    session=scratch3.Session(sessionID, username=uname)
+    conn=session.connect_cloud(ProjID)
+    conn.set_var(varname, value)
+    answer=input("Done? y/n NOT yes, no, Y, or N")
+    if answer=="y":
+        Done=1
+    conn.disconnect()
 # Test this at: https://scratch.mit.edu/projects/851418630/
 # If you use that, then for the project ID, use this: 851418630
