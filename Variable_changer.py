9a126bb29756f2ad07d4b78961595d16c85b95e7
htmlcoder1562 <nolanlemmon3@gmail.com>
2025-08-07 15:38:55 -0400

    Update Variable_changer.py

--- Variable_changer.py
+++ Variable_changer.py
@@ -2,11 +2,12 @@ import scratchattach as scratch3
 from getpass import getpass
 Done=0
 uname=input("What is your username?\n")
+pswd=getpass("What's your password for Scratch? (Hidden in the terminal)")
+ProjID=input("What is the project id?\n")
 while Done==0:
-    ProjID=input("What is the project id?\n")
     varname=input("What is the name of the cloud variable?\n")
     value=input("What is the value? note: numbers only\n")
-    session=scratch3.login(uname, getpass("What's your password for Scratch? (Hidden in the terminal)"))
+    session=scratch3.login(uname, pswd)
     conn=session.connect_cloud(ProjID)
     for x in ["1","2"]: # Repeated twice because the variables don't always change first thing
         conn.set_var(varname, value)
