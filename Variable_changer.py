3482dc37e33175be1095e53cdbd2b129db0f7c97
htmlcoder1562 <nolanlemmon3@gmail.com>
2025-08-07 15:11:35 -0400

    Update Variable_changer.py

--- Variable_changer.py
+++ Variable_changer.py
@@ -8,12 +8,11 @@ while Done==0:
     value=input("What is the value? note: numbers only\n")
     session=scratch3.Session(sessionID, username=uname)
     conn=session.connect_cloud(ProjID)
-    for x in ["1","2"]
+    for x in ["1","2"]: # Repeated twice because the variables don't always change first thing
         conn.set_var(varname, value)
-    answer=input("Done? y/n NOT yes, no, Y, or N")
+    answer=input("Done? (y/n)")
     if answer=="y":
         Done=1
     conn.disconnect()
 # Test this at: https://scratch.mit.edu/projects/851418630/
 # If you use that, then for the project ID, use this: 851418630
-# Also must have linux.
