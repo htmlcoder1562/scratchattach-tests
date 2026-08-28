fc7b6c0790e07d7a8cc3dd948f2004571d255bdf
htmlcoder1562 <110137761+htmlcoder1562@users.noreply.github.com>
2026-08-28 08:52:56 -0500

    Updated Event-test.py
    Creating and deleting variables isn't done with the project, removed that feature.

--- Event-test.py
+++ Event-test.py
@@ -11,14 +11,6 @@ def on_set(activity): #Called when a cloud var is set
     # `activity` is a sa.CloudActivity object
     # To get the user who set the variable, call activity.load_log_data() which saves the username to the activity.username attribute
 
-@events.event
-def on_del(activity):
-    print(f"{activity.user} deleted variable {activity.var}")
-
-@events.event
-def on_create(activity):
-    print(f"{activity.user} created variable {activity.var}")
-
 @events.event #Called when the event listener is ready
 def on_ready():
    print("Event listener ready!")
